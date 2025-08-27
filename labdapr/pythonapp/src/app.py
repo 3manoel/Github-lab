86669a40bf2cbd8ec28f12b0301139bc275311b5
emanoel.vieira <emanoel.vieira@outlook.com>
2025-08-27 01:19:58 -0300

    update-pythonapp

--- labdapr/pythonapp/src/app.py
+++ labdapr/pythonapp/src/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from controllers.orders_controller import OrdersController
+from services.order_service import OrderService
+from dapr.clients import DaprClient
 import sys
 
 app = Flask(__name__)
 
+# Initialize the Dapr client
+dapr_client = DaprClient()
+
+# Initialize the OrderService
+order_service = OrderService(dapr_client)
+
 # Initialize the OrdersController
-orders_controller = OrdersController()
+orders_controller = OrdersController(order_service)
 
 # Debug statement to print the Python module search paths
 print("PYTHONPATH:", sys.path)
