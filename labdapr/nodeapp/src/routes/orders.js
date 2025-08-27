6a9faa2d63517184660aa9517849b50c3c42bc64
emanoel.vieira <emanoel.vieira@outlook.com>
2025-08-27 00:39:32 -0300

    update-nodeapp

--- labdapr/nodeapp/src/routes/orders.js
+++ labdapr/nodeapp/src/routes/orders.js
@@ -4,6 +4,7 @@ const ordersController = require('../controllers/ordersController');
 const router = express.Router();
 
 // Route for handling new orders
-router.post('/neworder', ordersController.handleNewOrder);
+// use the function name actually exported by the controller (newOrder)
+router.post('/neworder', ordersController.newOrder);
 
 module.exports = router;
